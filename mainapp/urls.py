3462863fccee043caad11528b82eba7e3f7215d8
nehal6657 <nehalreshuaaryan@gmail.com>
2021-06-13 04:08:20 +0530

    added project

---
+++ mainapp/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('output',views.output,name='output'),
+    path('predicted',views.predicted,name='predicted'),
+    path('sendfile',views.sendfile,name='sendfile'),
+    path('login',views.login,name='login'),
+    path('register', views.register, name='register'),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT) 
